ab16f2b324a5d38dba448d096c54db6c56aa1e6a
mckidono <72043008+mckidono@users.noreply.github.com>
2021-01-24 21:36:28 -0600

    Update U1P1.java

--- Java/School/Java/U1P1.java
+++ Java/School/Java/U1P1.java
@@ -1,9 +1,32 @@
-package School.Java;
+package edu.wctc;
 
+import java.util.Scanner;
 
 public class Main {
-
+	public int scorecalc(int a, int b){
+		int sum = a+b;
+		return sum;
+	}
     public static void main(String[] args) {
-
+    String[] op1={"Mild","Tea","Breakfast","Summer","Paper"};
+    String[] op2={"Spicy","Coffee","Brunch","Winter","Plastic"};
+	System.out.println("1 is to the left and 0 is to the right");
+	Scanner sc= new Scanner(System.in);
+	int score=0;
+	Main obj= new Main();
+	for(int x=0;x<op1.length;x++) {
+		System.out.println(op1[x] + " or " + op2[x]);
+		int a = sc.nextInt();
+		score =obj.scorecalc(score, a);
+	}
+	if(score<3){
+		System.out.println("You prefer life to be calm and organized");
+	}
+	else if(score>3){
+		System.out.println("You prefer life to be spontaneous and active");
+	}
+	else{
+		System.out.println("You prefer a good balance in life.");
+	}
     }
 }
